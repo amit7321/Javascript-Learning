5d59b17b54972aeec11a5188dbe00e4c8dd1ed09
amit <amit.hasan7321@gmail.com>
2024-02-19 19:26:09 +0600

    add attack and strong atttack.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 15;
+const STRONG_ATTACK_VALUE = 20;
 
 let choseMaxLife = 100;
 let currentMonsterHealth = choseMaxLife;
@@ -6,12 +8,34 @@ let currentPlayerHealth = choseMaxLife;
 
 adjustHealthBars(choseMaxLife);
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
     if (currentMonsterHealth <= 0) {
         alert("tui jitsos mamu !!!!!!")
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("you draw!!");
+    } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
+        alert("you lost");
     }
+
+}
+
+function attackHandler() {
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
