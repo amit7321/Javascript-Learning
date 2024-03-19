824e7f1137e7a0f9878df8d000a3e584fff0b550
amit <amit.hasan7321@gmail.com>
2024-03-19 20:01:41 +0600

    Add some method in the game play.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,21 +3,42 @@ const MONSTER_ATTACK_VALUE = 15;
 const STRONG_ATTACK_VALUE = 20;
 const HEAL_VALUE = 20;
 
-let choseMaxLife = 100;
+const enteredVelue = prompt("maximum life for you", '100');
+
+let choseMaxLife = parseInt(enteredVelue);
+
+if(isNaN(choseMaxLife) || choseMaxLife <= 0) {
+    choseMaxLife = 100;
+}
+
 let currentMonsterHealth = choseMaxLife;
 let currentPlayerHealth = choseMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(choseMaxLife);
 
 function endround() {
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert("You would be dead but the bonus life saved you")
+    }
+
     if (currentMonsterHealth <= 0) {
-        alert("tui jitsos mamu !!!!!!")
+        alert("tui jitsos mamu !!!!!!");
+        reset();
     } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
         alert("you draw!!");
+        reset();
     } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
         alert("you lost");
+        reset();
     }
 }
 
@@ -43,7 +64,22 @@ function strongAttackHandler() {
 }
 
 function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth >= choseMaxLife - HEAL_VALUE) {
+        alert("You can not heal more than initial health");
+        healValue = choseMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
     increasePlayerHealth(HEAL_VALUE);
+    currentPlayerHealth += HEAL_VALUE;
+    endround();
+}
+
+function reset() {
+    currentMonsterHealth = choseMaxLife;
+    currentPlayerHealth = choseMaxLife;
+    resetGame();
 }
 
 attackBtn.addEventListener('click', attackHandler);
